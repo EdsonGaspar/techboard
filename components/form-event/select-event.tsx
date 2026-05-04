56564a8d0b170ca2b23b8927b890a9c07a06f01e
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-05-04 16:10:34 +0100

    update: Teste

--- components/form-event/select-event.tsx
+++ components/form-event/select-event.tsx
@@ -3,6 +3,7 @@ export function SelectFormEvento() {
     <select className="bg-[#212121] border border-[#4A4949] px-4 py-2 rounded-lg outline-none">
       <option value={""}>Primeiro tema</option>
       <option value={""}>Segundo tema</option>
+      <option value={""}>Segundo tema</option>
     </select>
   );
 }
