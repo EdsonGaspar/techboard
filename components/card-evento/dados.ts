fdd37078065246ae60f877e42d6027e5357c0316
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2026-05-06 16:12:41 +0100

    upDate: Alimentando os cards com dados.

---
+++ components/card-evento/dados.ts
@@ -0,0 +1,152 @@
+import mulheresFrontEnd from "../../assets/mulheres.png";
+import pixel from "../../assets/pixel.png";
+import backendmaster from "../../assets/backendmaster.png";
+import codetocode from "../../assets/codetocod.png";
+import serversidesubmmit from "../../assets/serversidesubmmit.png";
+import devopsevolution from "../../assets/devopsevolution.png";
+import deeplearning from "../../assets/deeplearning.png";
+import ianapratica from "../../assets/ianapratica.png";
+import iasegura from "../../assets/iaseguraeassecivel.png";
+import datapulse from "../../assets/datapulse.png";
+import datarevolution from "../../assets/datarevoution.png";
+import drivebydata from "../../assets/drivembydata.png";
+import sqlsubmit from "../../assets/sqlsubmmit.png";
+import skyteach from "../../assets/sky.png";
+import mundocloud from "../../assets/mundocloud.png";
+
+export const DadosCard = [
+  {
+    id: 1,
+    image: mulheresFrontEnd,
+    botao: "Front-End",
+    data: "20/02/2026",
+    titulo: " Mulheres no front-end",
+    descricao:
+      "Valorizando e impulsionando a participação feminina no desenvolvimento front-end.",
+  },
+  {
+    id: 2,
+    image: pixel,
+    botao: "Front-End",
+    data: "20/02/2026",
+    titulo: "Pixel & Code",
+    descricao:
+      "Meetup unindo design e desenvolvimento front-end para explorar tendências em UI.",
+  },
+  {
+    id: 3,
+    image: backendmaster,
+    botao: "Back-End",
+    data: "20/02/2026",
+    titulo: "Back-End Masters",
+    descricao:
+      "Performance e inovação no back-end, conectando especialistas e entusiastas.",
+  },
+  {
+    id: 4,
+    image: codetocode,
+    botao: "Back-End",
+    data: "20/02/2026",
+    titulo: "Code to Core",
+    descricao:
+      "Workshop que explora a essência do back-end, escalabilidade e boas práticas.",
+  },
+  {
+    id: 5,
+    image: serversidesubmmit,
+    botao: "Back-End",
+    data: "20/02/2026",
+    titulo: "Server Side Summit",
+    descricao:
+      "Evento dedicado a tecnologias e estratégias para desenvolvimento back-end.",
+  },
+  {
+    id: 6,
+    image: devopsevolution,
+    botao: "DevOps",
+    data: "20/02/2026",
+    titulo: "DevOps Evolution",
+    descricao:
+      "Congresso sobre automação, CI/CD, cultura DevOps, tendências e inovações.",
+  },
+  {
+    id: 7,
+    image: deeplearning,
+    botao: "Inteligência artificial",
+    data: "20/02/2026",
+    titulo: "Deep Learning Days",
+    descricao: "Imersão sobre IA, redes neurais e aprendizado profundo.",
+  },
+  {
+    id: 8,
+    image: ianapratica,
+    botao: "Inteligência artificial",
+    data: "20/02/2026",
+    titulo: "IA na palma da mão",
+    descricao:
+      "Workshop IA em dispositivos móveis, aplicando a tecnologia em soluções acessíveis.",
+  },
+  {
+    id: 9,
+    image: iasegura,
+    botao: "Inteligência artificial",
+    data: "20/02/2026",
+    titulo: "Inteligência artificial",
+    descricao:
+      "Palestra sobre segurança e inclusão em soluções baseadas em inteligência artificial.",
+  },
+  {
+    id: 10,
+    image: datapulse,
+    botao: "data science",
+    data: "20/02/2026",
+    titulo: "Data Pulse",
+    descricao:
+      "Congresso sobre dados nas decisões estratégicas, análise e inovações da ciência de dados.",
+  },
+  {
+    id: 11,
+    image: datarevolution,
+    botao: "data science",
+    data: "20/02/2026",
+    titulo: "Data Revolution",
+    descricao:
+      "Palestra sobre últimas tendências em big data, análise e inteligência empresarial.",
+  },
+  {
+    id: 12,
+    image: drivebydata,
+    botao: "data science",
+    data: "20/02/2026",
+    titulo: "Driven by Data",
+    descricao:
+      "Minicurso sobre como dados impulsionam decisões e inovações para o futuro.",
+  },
+  {
+    id: 13,
+    image: sqlsubmit,
+    botao: "data science",
+    data: "20/02/2026",
+    titulo: "SQL Summit",
+    descricao:
+      "Ciclo de palestras com especialistas do universo dos bancos de dados SQL.",
+  },
+  {
+    id: 12,
+    image: skyteach,
+    botao: "Cloud",
+    data: "20/02/2026",
+    titulo: "SkyTech Summit",
+    descricao:
+      "Palestra sobre tendências em computação em nuvem, escalabilidade e infraestrutura.",
+  },
+  {
+    id: 13,
+    image: mundocloud,
+    botao: "Cloud",
+    data: "20/02/2026",
+    titulo: "Mundo Cloud",
+    descricao:
+      "Encontro de especialistas e entusiastas para discutir novidades da tecnologia cloud.",
+  },
+];
